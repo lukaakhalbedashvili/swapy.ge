d8ce0d5127ed204b1df8a9a6269c699c61341624
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-06-19 20:08:59 +0400

    feat ( ID input )

--- src/app/failure/page.tsx
+++ src/app/failure/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div className="w-full h-screen bg-background">
+      <h1 className="text-error">ოპერაცია ვერ შესრულდა</h1>
+    </div>
+  );
 };
 
 export default page;
