e04b1786bedd8a9a12df89f838a100f00c7b307f
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-06-19 11:28:57 +0400

    fix ( api route handler to server actions )

--- src/server-actions/index.ts
+++ src/server-actions/index.ts
@@ -1,19 +1,47 @@
 "use server";
 
+import { getRequestBody } from "@/utils";
 import {
   CreatePaymentLinkResponseI,
   getPaymentLinkActionPayloadI,
 } from "./server_actions_types";
 
+const getAuthToken = async () => {
+  const res = await fetch(
+    "https://oauth2.bog.ge/auth/realms/bog/protocol/openid-connect/token",
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${process.env.BOG_AUTH_BASE64!}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+      }),
+    }
+  );
+
+  const data = await res.json();
+
+  return data.access_token;
+};
+
 export const getPaymentLinkAction = async ({
   requiredLariAmount,
   paymentMethod,
 }: getPaymentLinkActionPayloadI): Promise<CreatePaymentLinkResponseI> => {
-  const response = await fetch("http://localhost:3000/api/payments/sell", {
+  "use server";
+  const authToken = await getAuthToken();
+
+  const res = await fetch("https://api.bog.ge/payments/v1/ecommerce/orders", {
     method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ requiredLariAmount, paymentMethod }),
+    headers: {
+      "Accept-Language": "ka",
+      Authorization: `Bearer ${authToken}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(getRequestBody(paymentMethod, requiredLariAmount)),
   });
 
-  return await response.json();
+  return await res.json();
 };
