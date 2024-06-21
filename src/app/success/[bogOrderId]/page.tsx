1a60729d18135fed3c401db60d575b24aaa43bf0
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-06-21 09:00:34 +0400

    feat ( transaction success page )

--- src/app/success/[bogOrderId]/page.tsx
+++ src/app/success/[bogOrderId]/page.tsx
@@ -1,7 +1,27 @@
+import { getTransaction } from "@/server-actions";
+import Image from "next/image";
 import React from "react";
 
-const page = () => {
-  return <div>page</div>;
+interface SuccessPropsI {
+  params: { bogOrderId: string };
+}
+
+const page = async ({ params }: SuccessPropsI) => {
+  const transaction = await getTransaction(params.bogOrderId);
+  return (
+    <div className="w-full h-screen bg-background flex flex-col justify-center items-center">
+      <div className="bg-[#36930f] p-3 rounded-full w-fit">
+        <Image src="/checked.svg" width={50} height={50} alt="success" />
+      </div>
+
+      <h1 className="text-success mt-5 text-lg text-center">
+        გადახდილია , {Number(transaction?.amount).toFixed(2)}₾ ანგარიშზე 10
+        წუთის განმავლობაში დაგერიცხებათ
+      </h1>
+
+      <p className="text-main text-3xl mt-8 font-bold">50.0 ₾</p>
+    </div>
+  );
 };
 
 export default page;
