53dabcec17ce206d2107f6fb542d1a0caf4cfb2f
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-06-25 16:49:27 +0400

    feat ( og image )

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Contact from "@/sections/Contact";
 import FAQ from "@/sections/FAQ";
 import Team from "@/sections/Team";
 import Transactions from "@/sections/Transactions";
+import Image from "next/image";
 import Link from "next/link";
 
 {
@@ -15,18 +16,22 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center bg-background text-white">
-      <header className="flex items-center w-full px-4 bg-header sticky top-0 z-10">
+      <header className="flex items-center w-full px-4 bg-header sticky top-0 z-10 py-3">
         <nav className="w-full">
           <ul className="flex items-center w-full">
-            <li className="p-3">
+            <li>
+              <Image src="/swapy-logo.svg" width={40} height={40} alt="swapy" />
+            </li>
+
+            <li className="ml-4">
               <Link href="#გაცვალე">გაცვალე</Link>
             </li>
 
-            <li className="p-3">
+            <li className="ml-4">
               <Link href="#ჩვენს შესახებ">ჩვენს შესახებ</Link>
             </li>
 
-            <li className="p-3">
+            <li className="ml-4">
               <a href="#კონტაქტი">კონტაქტი</a>
             </li>
           </ul>
