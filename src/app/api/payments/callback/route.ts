f98ea4d8151a4cecf56763ccd2b3a0322dbda0c2
lukaakhalbedashvili <lukaakhalbedashvili@gmail.com>
2024-06-25 16:49:27 +0400

    feat ( og image )

--- src/app/api/payments/callback/route.ts
+++ src/app/api/payments/callback/route.ts
@@ -1,13 +1,43 @@
+import { NextResponse } from "next/server";
+import prisma from "../../../../../lib/prisma";
+
 export async function POST(req: Request) {
-  const request = await req.json();
+  try {
+    const request = await req.json();
 
-  console.error(
-    request.body.order_status,
-    "request.body.order_status",
-    request
-  );
+    console.error(
+      request.body.order_status.values,
+      "request.body.order_status",
+      request
+    );
 
-  return new Response("Hello, Next.js!", {
-    status: 200,
-  });
+    const orderId = request.body.order_id;
+
+    try {
+      const updatedOrder = await prisma.transaction.update({
+        where: {
+          orderIdBOG: orderId,
+        },
+        data: {
+          isPaid: request.body.order_status.values === "წარმატებული",
+        },
+      });
+
+      console.log(updatedOrder);
+
+      return new NextResponse("Order status updated successfully!", {
+        status: 200,
+      });
+    } catch (error) {
+      console.error("Error updating order status:", error);
+      return new NextResponse("Error updating order status", {
+        status: 500,
+      });
+    }
+  } catch (error) {
+    console.error("Error parsing request:", error);
+    return new NextResponse("Invalid request", {
+      status: 400,
+    });
+  }
 }
